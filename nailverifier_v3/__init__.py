6779c14a340b338c4aff85d3565c4f65c730dae8
hung <120816135+hungdeniubeo@users.noreply.github.com>
2026-09-25 15:47:08 +0700

    feat(v3): add nailverifier_v3/__init__.py

---
+++ nailverifier_v3/__init__.py
@@ -0,0 +1,3 @@
+from .engine import ENGINE_VERSION, OFFICIAL_ADAPTERS, VerificationEngine, verify_dataframe
+
+__all__ = ["ENGINE_VERSION", "OFFICIAL_ADAPTERS", "VerificationEngine", "verify_dataframe"]
